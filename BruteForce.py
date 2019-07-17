203d3b687676cd21c24e3c55f3f53c01cb8c22c0
bhertzman <40404772+bhertzman@users.noreply.github.com>
2019-07-17 11:18:09 -0500

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,35 @@
+let = input("enter letter ")
+
+def encryption(letter, shift): 
+    num = ord(letter)
+    letter = (num-97)
+    pt= (letter+shift)%26
+    
+    pt = pt+97
+    pt = chr(pt)
+    return pt
+    print(chr((letter+100)%122))
+      
+def caes2(word):
+    et = ""
+    for x in word:
+        et = et.__add__(encryption(x, 3))
+    print(et)
+def decryption(letter, shift): 
+    num = ord(letter)
+    letter = (num-97)
+    pt= (letter-shift)%26
+    
+    pt = pt+97
+    pt = chr(pt)
+    return pt
+    print(chr((letter+100)%122))
+def bfa(ct):
+    for shift in range(1,26):
+       
+        dle = ""
+        for x in ct:
+            dl = ((ord(x)-97) - shift)%26
+            dle = dle.__add__(chr(dl+97))
+        print(dle)
+bfa(let)
